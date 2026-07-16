c688c92eab7b4747f23302d52c94482f3b316f4e
Tchonchoko Kagbere <hkagbere3@gmail.com>
2026-07-16 14:55:00 +0200

    corrections d'orthographe sur la classe TacoOrder

--- src/main/java/com/huberto/tacocloud/domain/TacoOrder.java
+++ src/main/java/com/huberto/tacocloud/domain/TacoOrder.java
@@ -9,12 +9,12 @@ import java.util.List;
 public class TacoOrder {
     private String deliveryName;
     private String deliveryStreet;
-    private String deliveryCCity;
+    private String deliveryCity;
     private String deliveryState;
     private String deliveryZip;
     private String ccNumber;
     private String ccExpiration;
-    private String ccVV;
+    private String ccCVV;
 
     private List<Taco> tacos = new ArrayList<>();
     public void addTaco(Taco taco){
