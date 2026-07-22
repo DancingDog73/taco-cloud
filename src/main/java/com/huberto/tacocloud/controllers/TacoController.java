4685a2b294d18066241d93ee961d75204f7a364d
Tchonchoko Kagbere <hkagbere3@gmail.com>
2026-07-22 10:23:07 +0200

    ajout d'un bean DataLoader pour les tacos et service des tacos par id

--- src/main/java/com/huberto/tacocloud/controllers/TacoController.java
+++ src/main/java/com/huberto/tacocloud/controllers/TacoController.java
@@ -4,10 +4,11 @@ import com.huberto.tacocloud.domain.Taco;
 import com.huberto.tacocloud.repositories.TacoRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos", produces = "application/json")
@@ -25,6 +26,15 @@ public class TacoController {
         PageRequest page = PageRequest.of(
                 0, 12, Sort.by("createdAt").descending());
         return repository.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable ("id") Long id){
+        Optional<Taco> opTaco = repository.findById(id);
+        if(opTaco.isPresent()){
+            return ResponseEntity.ok(opTaco.get());
+        }
+        return  ResponseEntity.notFound().build();
     }
 
 
